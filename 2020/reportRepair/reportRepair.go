db9dbe9772571dc2b67dee8122a3e1d284269d62
Henry Mortimer <henry@morti.net>
2020-12-02 20:48:30 +0000

    refactor(report repair): reuse solve functionality

--- 2020/reportRepair/reportRepair.go
+++ 2020/reportRepair/reportRepair.go
@@ -15,31 +15,21 @@ func Solve(data io.Reader) (int, error) {
 		return 0, err
 	}
 
-	set := make(map[int]bool)
-	for _, expense := range expenses {
-		goal := SUM - expense
-		if _, ok := set[goal]; ok {
-			return goal * expense, nil
-		} else {
-			set[expense] = true
-		}
-	}
-
-	return 0, nil
+	return solve(expenses, SUM), nil
 }
 
-func solve(expenses []int, max int) (int, error) {
+func solve(expenses []int, max int) int {
 	set := make(map[int]bool)
 	for _, expense := range expenses {
 		goal := max - expense
 		if _, ok := set[goal]; ok {
-			return goal * expense, nil
+			return goal * expense
 		} else {
 			set[expense] = true
 		}
 	}
 
-	return 0, nil
+	return 0
 }
 
 func SolveTriple(data io.Reader) (int, error) {
@@ -53,10 +43,7 @@ func SolveTriple(data io.Reader) (int, error) {
 	for _, expense := range expenses {
 		goal := SUM - expense
 		i := sort.SearchInts(expenses, goal)
-		res, err := solve(expenses[:i], goal)
-		if err != nil {
-			return 0, err
-		}
+		res := solve(expenses[:i], goal)
 
 		if res != 0 {
 			return expense * res, nil
